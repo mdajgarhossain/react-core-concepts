33f685400785c40227455b6670ff49420059ec52
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-03-13 14:36:15 +0600

    Create component, return multiple HTML from a component, style a component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: 'Jewell',
+    job: 'Software Developer'
+  };
+  var style = {
+    color: 'maroon',
+    backgroundColor: 'yellow'
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 style={{color: 'green'}}>{`${person.job}`}</h1>
+        <h2 style={style}>Web Development</h2>
+        <Developer />
+        <Developer />
       </header>
+    </div>
+  );
+}
+
+function Developer() {
+  return(
+    <div style={{border: '2px solid green', margin: '10px', padding: '10px'}}>
+      <h2>A.H. Jewell</h2>
+      <h3>React Developer</h3>
     </div>
   );
 }
