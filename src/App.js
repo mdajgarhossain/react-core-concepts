db1ed7a821614748272ecea6d383ce4a40d27de0
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-03-13 15:54:49 +0600

    Pass object to component as props

--- src/App.js
+++ src/App.js
@@ -3,27 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const developers = ['Fullstack', 'Frontend', 'Backend'];
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'}
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Developer name='A.H. Jewell' job='Software'/>
-        <Developer name='Jhankar Mahbub' job='React'/>
-        <Developer name='Karim Sheikh' job={developers[0]}/>
-        <Developer name='Ajgar' job={developers[1]}/>
+        {/* <Product name={products[0].name} price={products[0].price}/> */}
+        <Product product={products[0]}/>
+        <Product product={products[1]}/>
       </header>
     </div>
   );
 }
 
-function Developer(props) {
-  console.log(props);
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    color: 'blue',
+    height: '200px',
+    width: '200px',
+    float: 'left',
+    margin: '10px'
+  };
+
+  //Destructuring from props
+  const {name, price} = props.product;
+  console.log(name, price);
   
   return(
-    <div style={{border: '2px solid green', margin: '10px', padding: '10px'}}>
-      <h2>{props.name}</h2>
-      <h3>{props.job} Developer</h3>
+    <div style={productStyle}>
+      {/* <h3>{props.product.name}</h3>
+      <h4>{props.product.price}</h4> */}
+      <h3>{name}</h3>
+      <h4>{price}</h4>
+      <button>Buy Now</button>
     </div>
   );
 }
